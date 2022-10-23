d157da768f8cc5ecc081ee6d7c6086a1b57b0cfa
chipslays <hello@chipslays.ru>
2022-10-23 12:15:48 +0400

    change offset and limit

--- examples/page-list-pagination.php
+++ examples/page-list-pagination.php
@@ -9,7 +9,7 @@ $client = new Client;
 
 $token = '0baab53f5dc2ac3a3ec96253a634224eb63e908dd2e00aa082a245e3fcb9';
 
-$client->getPageListWithPagination($token, 20000, 200, function (Page $page) use ($client) {
+$client->getPageListWithPagination($token, 0, 50, function (Page $page) use ($client) {
     $details = $client->getPage($page, true);
 
     $text = $details->getText();
