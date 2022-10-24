e4b449682026ef5aa47855d66ba011b2e4ab31be
chipslays <hello@chipslays.ru>
2022-10-24 08:19:20 +0400

    update example

--- examples/page-list-pagination.php
+++ examples/page-list-pagination.php
@@ -10,9 +10,9 @@ $client = new Client;
 $token = '0baab53f5dc2ac3a3ec96253a634224eb63e908dd2e00aa082a245e3fcb9';
 
 $client->getPageListWithPagination($token, 0, 50, function (Page $page) use ($client) {
-    $details = $client->getPage($page, true);
+    $details = $client->getPage($page, true); // get content of page
 
-    $text = $details->getText();
+    $text = $details->getText(); // get text (its custom method)
 
-    echo $text;
+    echo $text . PHP_EOL;
 });
