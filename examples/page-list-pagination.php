1bf1f4e8d1b60164901e368906d7fe2ca891046d
chipslays <hello@chipslays.ru>
2023-01-27 21:37:14 +0400

    add comment

--- examples/page-list-pagination.php
+++ examples/page-list-pagination.php
@@ -9,6 +9,7 @@ $client = new Client;
 
 $token = '0baab53f5dc2ac3a3ec96253a634224eb63e908dd2e00aa082a245e3fcb9';
 
+// iteration of all pages until the last one
 $client->getPageListWithPagination($token, offset: 0, limit: 50, function (Page $page) use ($client) {
     $details = $client->getPage($page, true); // get content of page
 
